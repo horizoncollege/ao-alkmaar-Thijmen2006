83e9464c7830d1915854348060294e433ff6007d
Thijmen2006 <169696@student.horizoncollege.nl>
2023-02-09 13:50:05 +0100

    dingen aangepast

--- index.php
+++ index.php
@@ -3,18 +3,21 @@
 
 <head>
     <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
-<body>
+<body style="background-image: url('afbeeldingen/background.jpg'); color: white;">
 
-    <div class="bg-warning ">
+<div class="intro">
+    <div class="bg-primary text-center" style="border-radius: 30px; margin:20px">
         <h1>Application Dashboard</h1>
         <br>
-        <p>Hallo, Ik ben Thijmen en dit is mijn dashboard. Hier kun je naar al mijn projecten gaan die ik heb gemaakt tijdens mijn opleiding.</p>
+        <p>Hallo, ik ben Thijmen Hekkema en dit is mijn dashboard. Hier kun je naar al mijn projecten gaan die ik heb gemaakt tijdens mijn opleiding.</p>
         <br>
     </div>
-
+</div>
+<br>
     
     <div class="container">
         <div class="row">
@@ -41,6 +44,21 @@
             </div>
         </div>
     </div>
+
+    <footer class="bg-primary" style="display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    position: relative;
+    bottom: -197px;
+    width: 100%;
+    max-width: 7000px;
+    border-radius: 30px;">
+        <ul>
+            <p>© copyright all rights reserved</p>
+        </ul>
+    </footer>
+
 
 </body>
 
